18e6c6cd941a6f64c5e35986b62c00c1ef99900a
patrykmajchrzycki <patryk.majchrzycki@gmail.com>
2023-08-28 21:47:21 +0200

    #862k8wtzz Initialise Opportunity Service

---
+++ src/main/java/com/estating/opportunity/OpportunitySvcApplication.java
@@ -0,0 +1,13 @@
+package com.estating.opportunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunitySvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpportunitySvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estating/opportunity/OpportunitySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estating.opportunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportunitySvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
